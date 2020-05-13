66e21b199c3d29f459171c3ce6614bfec6141173
luisangel1209 <56208306+luisangel1209@users.noreply.github.com>
2020-05-13 11:06:41 -0500

    Diseño de Interfaces

--- app/src/main/java/com/example/appancito/Principal.java
+++ app/src/main/java/com/example/appancito/Principal.java
@@ -2,7 +2,9 @@ package com.example.appancito;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Principal extends AppCompatActivity {
 
@@ -11,4 +13,18 @@ public class Principal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
     }
+
+    public void Ayuda(View view){
+        Intent ayuda = new Intent(Principal.this, Ayuda.class);
+        startActivity(ayuda);
+    }
+    public void Contacto(View view){
+        Intent contacto = new Intent(Principal.this, Ayuda.class);
+        startActivity(contacto);
+    }
+    public void Pedidos(View view){
+        Intent pedidos = new Intent(Principal.this, Ayuda.class);
+        startActivity(pedidos);
+    }
 }
+
