34bb9595c4994cb12ac2d1a2750167cd089db65d
AlejandroPedrazaViveros <56176998+AlejandroPedrazaViveros@users.noreply.github.com>
2020-05-15 14:10:47 -0500

    Pantalla Ayuda Pantalla de Pago Pantalla datos de entrega ALEJANDRO Arregladas las pantallas de Miguel

---
+++ app/src/main/java/com/example/appancito/Contacto.java
@@ -0,0 +1,21 @@
+package com.example.appancito;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Contacto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacto);
+    }
+
+    public void Regresar(View view){
+        Intent regresar = new Intent(Contacto.this, Principal.class);
+        startActivity(regresar);
+    }
+}
