22785dc4bc63bdcd01bb44d7006a4c851a46f9ef
AlejandroPedrazaViveros <56176998+AlejandroPedrazaViveros@users.noreply.github.com>
2020-05-15 13:43:46 -0500

    Pantalla Ayuda Pantalla de Pago Pantalla datos de entrega ALEJANDRO Arregladas las pantallas de Miguel

--- app/src/main/java/com/example/appancito/PedidoPequenio.java
+++ app/src/main/java/com/example/appancito/PedidoPequenio.java
@@ -2,7 +2,9 @@ package com.example.appancito;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PedidoPequenio extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class PedidoPequenio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedido_pequenio);
     }
+
+    public void Regresar(View view){
+        Intent regresar = new Intent(PedidoPequenio.this, Login.class);
+        startActivity(regresar);
+    }
+
 }
